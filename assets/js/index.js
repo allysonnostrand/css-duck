eaffd26bae3aaa6f8a214b13a384f63bc5813a57
iggy <allyson.nostrandd@gmail.com>
2022-08-10 12:52:44 -0700

    added buttons for changing duck colors

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+let duckHead = document.getElementById('duckHead');
+let duckBody = document.getElementById('duckBody');
+let duckWing = document.getElementById('duckWing');
+
+console.log("linked")
+
+document.getElementById('pinkBtn').addEventListener('click', e => {
+    duckHead.style.backgroundColor = '#fce1e4';
+    duckBody.style.backgroundColor = '#fce1e4';
+    duckWing.style.backgroundColor = '#fce1e4';
+})
+
+document.getElementById('blueBtn').addEventListener('click', e => {
+    duckHead.style.backgroundColor = '#daeaf6';
+    duckBody.style.backgroundColor = '#daeaf6';
+    duckWing.style.backgroundColor = '#daeaf6';
+})
+
+document.getElementById('greenBtn').addEventListener('click', e => {
+    duckHead.style.backgroundColor = '#ddedea';
+    duckBody.style.backgroundColor = '#ddedea';
+    duckWing.style.backgroundColor = '#ddedea';
+})
+
+document.getElementById('whiteBtn').addEventListener('click', e => {
+    duckHead.style.backgroundColor = '#fce1e4';
+    duckBody.style.backgroundColor = '#fce1e4';
+    duckWing.style.backgroundColor = '#fce1e4';
+})
+
+document.getElementById('brownBtn').addEventListener('click', e => {
+    duckHead.style.backgroundColor = '#eddcd2';
+    duckBody.style.backgroundColor = '#eddcd2';
+    duckWing.style.backgroundColor = '#eddcd2';
+})
+
+
